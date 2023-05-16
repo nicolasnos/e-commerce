3bbc23100bb4b2e8861609245aabf62a65a928d6
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-05-16 16:40:56 -0500

    mapeo en la pagina de hombres

--- src/pages/Men.tsx
+++ src/pages/Men.tsx
@@ -1,11 +1,40 @@
-const api = fetch('https://fakestoreapi.com/products/')
-.then(res=>res.json())
-.then(json=>console.log(json));
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
 
-console.log(api)
+
 const Men = () => {
+  const [info, setInfo] = useState([])
+  useEffect(() => {
+    showApi()
+  }, [])
+  
+  const showApi =async () => {
+    const res = await fetch("https://fakestoreapi.com/products/")
+    const data = await res.json();
+    setInfo(data)
+    console.log(data)
+  }
+  
+  const itemsM = info.map((item: {title: string, description: string, image: string, price: number, category: string}, i: number)=>
+  <Card style={{ width: '18rem', margin: '3rem' }} key={i}>
+      <Card.Img variant="top" src={item.image} />
+      <Card.Body>
+        <Card.Title>{item.title}</Card.Title>
+        <Card.Text>
+          {item.description}
+        </Card.Text>
+        <Badge bg="light" text="dark">{item.price}</Badge>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+
   return (
-    <div>Men</div>
+    <section className='Men'>
+      {itemsM}
+    </section>
   )
 }
 
